a68d0b340955dcba6f30613259d4d6e80b8cbf90
sbiswajitkumar2001 <sbiswajitkumar2001@gmail.com>
2023-11-03 19:00:12 +0530

    today practice

---
+++ JAVA/Exercise.java
@@ -0,0 +1,13 @@
+public class Exercise {
+  public static void main(String [] args){
+    System.out.println("output is" + 9);
+    System.out.println("output is" + "mikun mg");
+    System.out.println("output is" + 'g');
+    System.out.println("output is" +9.5 );
+    System.out.println("output is" + false);
+    System.out.println(11 + 12);
+    System.out.println(11 + "mikun mg");
+    System.out.println("mikun mg" + "biswa mg");
+    System.out.println("mg" + 1.5);
+  }
+}
