5a31ad68d46fc4d49290878ccb8bcc2bebb4e172
mikun2000 <mikunmohanty4@gmail.com>
2023-11-03 19:00:34 +0530

    today exercises

---
+++ JAVA/Exercise.java
@@ -0,0 +1,12 @@
+class Exercise {
+    public static void main(String[] args) {
+        System.out.println("out put is:" + 12);
+        System.out.println("out put is:" + "mikun");
+        System.out.println("out put is:" + 'm');
+        System.out.println("out put is:" + 12.52);
+        System.out.println("out put is:" + false);
+        System.out.println(11 + 12);
+        System.out.println("lion" + "tiger");
+    }
+
+}
--- JAVA/practice.java
+++ JAVA/practice.java
@@ -1,4 +1,4 @@
-class Practice{
+class Practice {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
