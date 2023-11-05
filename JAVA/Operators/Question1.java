14fca60ff3ca77778a0e651ec6d4e7eb4b143a80
mikun2000 <mikunmohanty4@gmail.com>
2023-11-05 20:47:21 +0530

    arithmatic operator

---
+++ JAVA/Operators/Question1.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 7;
+        System.out.println(2 * (length + width));
+    }
+}
