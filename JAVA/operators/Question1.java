b2ce57a994496067e1a9c208c1069cdede05755b
sbiswajitkumar2001 <sbiswajitkumar2001@gmail.com>
2023-11-06 02:17:34 +0530

    practice on arethmatic operators

---
+++ JAVA/operators/Question1.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class Question1 {
+    public static void main(String[] args){
+        int length=5;
+        int weidgth=7;
+        System.out.println(2*(length+weidgth));
+    }
+    
+}
---
+++ JAVA/operators/Question2.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class Question2 {
+    public static void main(String[] args){
+        int num1=8;
+        int num2=2345;
+        System.out.println(((((num1+num2))/3)%5)*5);
+    }
+    
+}
