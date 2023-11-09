86aefb4ce73c1f0a8f89ffc8d6225ca71ceb3be4
soumya2000 <mikunmohanty4@gmail.com>
2023-11-09 14:59:58 +0530

    string exercise day3

---
+++ JAVA/String/Question4.java
@@ -0,0 +1,18 @@
+package String;
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String args[]){
+        // String word = "Umbrella";
+        // System.out.println(word.indexOf("e"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter a word ");
+        String wordName = sc.nextLine();
+        System.out.println("please enter a word to search ");
+        String searchWordName = sc.nextLine();
+
+        System.out.println("The word " + searchWordName + " is in the position of " + wordName.indexOf(searchWordName));
+
+    }
+    
+}
